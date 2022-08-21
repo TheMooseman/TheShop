38469cc63aec8a7faea30a521077018fd1262027
skylermoosman@gmail.com <skylermoosman@gmail.com>
2022-08-21 15:59:48 -0700

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import '../styles/Navbar.scss'
 import { ShoppingCart } from '@mui/icons-material';
 
